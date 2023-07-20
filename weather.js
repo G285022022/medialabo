b3e656de27023be2e4ebbcc727cbc418d73ff8d2
Furukawa　Shoei <228502@st.takushoku-u.ac.jp>
2023-07-20 16:42:55 +0900

    チンポ

---
+++ weather.js
@@ -0,0 +1,17 @@
+let searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', iroiro);
+
+function iroiro() {
+  let cityId = document.getElementById('city-id').value.trim(); // Add trim() to remove any leading/trailing spaces
+  if (cityId === '') {
+    console.log('都市のIDを入力してください'); // Log an error message if the input is empty
+    return;
+  }
+
+  let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
+
+  axios.get(url)
+    .then(showResult)
+    .catch(showError)
+    .then(finish);
+}
